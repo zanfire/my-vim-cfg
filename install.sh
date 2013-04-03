58c4843e83362a462c94ba4005092082cd5526f8
Matteo Valdina <zanfire@gmail.com>
2013-04-03 18:29:35 +0200

    A first version of install script.

--- install.sh
+++ install.sh
@@ -3,24 +3,58 @@
 # Script that install the checkout as a configuration of vi.
 # Author: Matteo Valdina
 
-echo This scripts will substitute .vim folder and .vimrc file with this configuration file and plugins collection.
+echo "-------------------------"
+echo "| INSTALL script        |"
+echo "-------------------------"
+echo " "
+echo "This script will substitute .vim folder and .vimrc file with this configuration file and plugins collection."
+echo " "
+
+read -p "Are you sure? (Y/n) " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]] ; then
+  echo " ..."
+else
+  echo "Exit from script."
+  echo " "
+  exit
+fi
+
 
 # Get install.sh directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-touch ~/test.file
+VIMRC=~/.vimrc
+VIMDIR=~/.vim
 
-# Check if .vim folder exists and ask if to be deleted or to be moved.
-if [ -f ~/test.file ] ; then
-  rm -i ~/test.file
-  echo $?
+# Check if .vimrc folder exists and ask if to be deleted or to be moved.
+if [ -e "$VIMRC" ] ; then
+  rm -i "$VIMRC"
+  if [ -e "$VIMRC" ] ; then
+    echo "Install script aborted."
+    exit
+  fi
 fi
 
-exit
+# Check if .vim folder exists and ask if to be deleted or to be moved.
+if [ -e "$VIMDIR" ] ; then
+  rm -IR "$VIMDIR"
+  if [ -e "$VIMRC" ] ; then
+    echo "Install script aborted."
+    exit
+  fi
+fi
 
-# Check if .vimrc file exists and ask if to be deleted or to be moved.
+echo " ... "
 
+
+pushd ~ > /dev/null
 ln -s $DIR/vimrc .vimrc
+echo "Linking $DIR/vimrc <- ~/.vimrc"
 ln -s $DIR .vim
+echo "Linking $DIR <- ~/.vim"
+popd > /dev/null
 
+echo " "
+echo "Done."
 
